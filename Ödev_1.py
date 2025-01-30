66bfc25aca887d66f4b479b12f6f4eb8fef351cd
Gul-yilmaz <yilmazlutfiyegul@gmail.com>
2025-01-31 01:38:34 +0300

    Ödev 1 ve Ödev 2 güncellendi

---
+++ Ödev_1.py
@@ -0,0 +1,15 @@
+# Sayısal veriyi stringe çevirme
+sayi = 42
+sayi_str = str(sayi)
+
+# Stringi tam sayıya çevirme
+metin = "123"
+metin_int = int(metin)
+
+# Sayıyı ondalıklı hale çevirme
+sayi_float = float(sayi)
+
+# Çıktıları yazdıralım
+print(type(sayi_str), sayi_str)  # <class 'str'> 42
+print(type(metin_int), metin_int)  # <class 'int'> 123
+print(type(sayi_float), sayi_float)  # <class 'float'> 42.0
